9c368e08273d5b224021294704b4b281ac285c14
Jari Takkala <jtakkala@gmail.com>
2013-12-17 16:17:27 -0500

    Handle different JSON messages and comment out test code

--- content/lib/ui.js
+++ content/lib/ui.js
@@ -1,42 +1,84 @@
 var socket = new WebSocket("ws://localhost:8080/ws", "protocolOne");
+
+var data = [[5, 12], [2, 9], [3,4]]
 socket.onmessage = function(msg) {
-	console.log(msg.data);
+	var json = JSON.parse(msg.data);
+	if (json.Stats) {
+		//updateStats(json);
+	} else {
+		console.log(json);
+	}
 };
 socket.onclose = function() {
 	console.log("Closed damn it!")
 };
 
 /*
-var w = 800;
-var h = 600;
-var force = d3.layout.force()
-	.nodes(dataset.nodes)
-	.links(dataset.edges)
-	.size([w, h])
-	.start();
+var margin = {top: 20, right: 20, bottom: 20, left: 40},
+	width = 300 - margin.left - margin.right,
+	height = 200 - margin.top - margin.bottom;
 
-var svg = d3.select("body").append("svg")
-	.attr("width", w)
-    .attr("height", h);
-*/
+var x = d3.scale.linear()
+	.domain([0, d3.max(data, function(d) { return d[0] })])
+	.range([0, width]);
+ 
+var y = d3.scale.linear()
+	.domain([0, d3.max(data, function(d) { return d[1] })])
+	.range([height, 0]);
 
+var line = d3.svg.line()
+	.x(function(d, i) { return x(d[0]); })
+	.y(function(d, i) { return y(d[1]); });
 
-/*
-var svg = d3.select("body")
-			.append("svg")
-			.attr("width", 600)
-			.attr("height", 400);
+//var width = d3.select("#throughput").style("width");
+//var height = d3.select("#throughput").style("height");
 
-var n = 100
-var max = Math.ceil(Math.sqrt(n))
-for (var row = 0; row < max; row++) {
-	for (var col = 0; col < max; col++) {
-		var rect = svg.append("rect")
-			.attr("x", col * 20 + 5)
-			.attr("y", row * 20 + 5)
-			.attr("height", 10)
-			.attr("width", 10)
-			.attr("fill", "black");
+var svg = d3.select("#throughput").append("svg")
+	.attr("width", width + margin.left + margin.right)
+	.attr("height", height + margin.top + margin.bottom)
+	.append("g")
+		.attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+svg.append("defs").append("clipPath")
+	.attr("id", "clip")
+	.append("rect")
+		.attr("width", width)
+		.attr("height", height);
+
+svg.append("g")
+	.attr("class", "x axis")
+	.attr("transform", "translate(0," + y(0) + ")")
+	.call(d3.svg.axis().scale(x).orient("bottom"));
+
+svg.append("g")
+	.attr("class", "y axis")
+	.call(d3.svg.axis().scale(y).orient("left"));
+
+var path = svg.append("g")
+	.attr("clip-path", "url(#clip)")
+	.append("path")
+		.datum(data)
+			.attr("class", "line")
+			.attr("d", line);
+
+function updateStats(json) {
+	if (!json.Stats) {
+		return
 	}
+	data.push([json.Stats.DownloadRate, Date.now()]);
+	if (data.length > 20) {
+		data.pop()
+	}
+	// redraw the line, and slide it to the left
+	path
+		.attr("d", line)
+		.attr("transform", null)
+		.transition()
+			.duration(500)
+			.ease("linear")
+			.attr("transform", "translate(" + x(-1) + ",0)")
+			.each("end", updateStats)
+		.call(d3.svg.axis().scale(x).orient("bottom"));
+	console.log(data)
 }
 */
