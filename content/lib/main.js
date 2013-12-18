957ceaf1edb208c04e242d3be1df7b346dcede56
Jari Takkala <jtakkala@gmail.com>
2013-12-18 11:41:03 -0500

    Add throughput.js and rename ui.js to main.js

---
+++ content/lib/main.js
@@ -0,0 +1,32 @@
+var socket = new WebSocket("ws://localhost:8080/ws", "protocolOne");
+
+initializeDiGraph();
+initThroughputChart();
+
+socket.onopen = function() {
+	console.log("SOCKET OPENED");
+}
+
+socket.onmessage = function(msg) {
+	console.log(msg.data)
+	var json = JSON.parse(msg.data);
+	if (json.Stats) {
+		console.log("Received Stats data");
+		handleStatsMessage(json.Stats);
+	} else if (json.DiGraph) {
+		console.log("Received DiGraph data");
+		handleDiGraphMessage(json.DiGraph);
+	} else if (json.Progress) {
+		console.log("Received Progress data");
+		handleProgressMessage(json.Progress);
+	} else if (json.Piece) {
+		console.log("Received Piece data");
+		handlePieceMessage(json.Piece);
+	} else {
+		console.log("Received JSON of unknown type");
+		console.log(json);
+	}
+};
+socket.onclose = function() {
+	console.log("SOCKET CLOSED");
+};
--- content/lib/throughput.js
+++ content/lib/throughput.js
@@ -1,35 +1,3 @@
-var socket = new WebSocket("ws://localhost:8080/ws", "protocolOne");
-
-initializeDiGraph()
-
-socket.onopen = function() {
-	console.log("SOCKET OPENED")
-}
-
-var data = [[5, 12], [2, 9], [3,4]]
-socket.onmessage = function(msg) {
-	console.log(msg.data)
-	var json = JSON.parse(msg.data);
-	if (json.Stats) {
-		//updateStats(json);
-	} else if (json.DiGraph) {
-		console.log("Received DiGraph data")
-		handleDiGraphMessage(json.DiGraph)
-	} else if (json.Progress) {
-		console.log("Received Progress data")
-		handleProgressMessage(json.Progress)
-	} else if (json.Piece) {
-		console.log("Received Piece data")
-		handlePieceMessage(json.Piece)
-	} else {
-		console.log("Received JSON of unknown type")
-		console.log(json);
-	}
-};
-socket.onclose = function() {
-	console.log("SOCKET CLOSED")
-};
-
 /*
 var margin = {top: 20, right: 20, bottom: 20, left: 40},
 	width = 300 - margin.left - margin.right,
