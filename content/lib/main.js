14de9777cb158d3a69df1e811515935d6b2e96a2
Brian Dignan <briandignan@gmail.com>
2013-12-19 14:35:45 -0500

    Remove some console logging from JS files

--- content/lib/main.js
+++ content/lib/main.js
@@ -12,16 +12,16 @@ socket.onmessage = function(msg) {
 	console.log(msg.data)
 	var json = JSON.parse(msg.data);
 	if (json.Stats) {
-		console.log("Received Stats data");
+		//console.log("Received Stats data");
 		updateStats(json.Stats);
 	} else if (json.DiGraph) {
-		console.log("Received DiGraph data");
+		//console.log("Received DiGraph data");
 		handleDiGraphMessage(json.DiGraph);
 	} else if (json.Progress) {
-		console.log("Received Progress data");
+		//console.log("Received Progress data");
 		handleProgressMessage(json.Progress);
 	} else if (json.Piece) {
-		console.log("Received Piece data");
+		//console.log("Received Piece data");
 		handlePieceMessage(json.Piece);
 	} else {
 		console.log("Received JSON of unknown type");
